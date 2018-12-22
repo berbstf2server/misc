9b7e975c2105e80fa67608db7da13c4427bb1ade
b e r b <berb.cgas@gmail.com>
2018-12-21 19:03:11 -0500

    updated main.cpp

--- flash-cards/main.cpp
+++ flash-cards/main.cpp
@@ -12,18 +12,20 @@ int main()
 	char answers[cardNumber][20] = {'\0'};
 
 	int currentQuestion = 0;
+	
 
-	input:
+	questionInput:
 	if (currentQuestion + 1 > cardNumber)
-		goto test;
+		goto answerInput;
 	else
 		cout << "Enter A question for card #" << currentQuestion + 1 << " (maximum 20 characters): ";
 		cin >> questions[currentQuestion];
 		cout << "Enter an answer for '" << questions[currentQuestion] << "':";
 		cin >> answers[currentQuestion];
 		currentQuestion = ++currentQuestion;
-		goto input;
+		goto questionInput;
 	
-	test:
+	answerInput:
+
 	return 0;
 }
