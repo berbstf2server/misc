707ebe98341e1ada521f2993b7a7f3e7a420c717
b e r b <berb.cgas@gmail.com>
2019-01-12 23:51:13 -0500

    Added testing folders

---
+++ roguelike/testing/ncurses-test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <ncurses.h>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+	const int ROWS = 5;
+	const int COLUMNS = 5;
+
+	char test[ROWS][COLUMNS] = {
+	{'#', '#', '#', '#', '#'},
+	{'#', '.', '.', '.', '#'},
+	{'#', '.', '.', '.', '#'},
+	{'#', '.', '.', '.', '#'},
+	{'#', '#', '#', '#', '#'}
+	};
+
+	initscr();	
+	curs_set(0);
+
+	for (int row = 0; row < ROWS; ++row)
+	{
+		printw("\n");
+		for (int column = 0; column < COLUMNS; ++column)
+		{
+			printw("%c", test[row][column]);	
+		}
+	}
+	
+	refresh();
+	getch();
+
+	endwin();
+	return 0;
+}
